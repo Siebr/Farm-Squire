8ee937ae3582276f7cb49800e76d21cd56686aec
Siebr <150930146+Siebr@users.noreply.github.com>
2023-12-15 16:14:50 +0100

    first prototype done

--- utility_functions.py
+++ utility_functions.py
@@ -8,6 +8,24 @@ import global_data as gd
 import animal_lifecycle_functions as al
 
 def assign_bedding(harvest_stores, animals_on_farm):
+    """
+    Determine how much bedding the herd needs.
+
+    Parameters
+    ----------
+    harvest_stores : pd.Series
+        Contains all harvested crops and their stored amounts.
+    animals_on_farm : pd.Series
+        Keeps track of which animals are on the farm and in what amount they
+        are present.
+
+    Returns
+    -------
+    bedding_used : pd.Series
+        Contains all crops suitable for bedding, and in which amount they are
+        used thusly.
+
+    """
     bedding_crops = harvest_stores.where(gd.plant_data['bedding_use'] == True)
     bedding_crops = bedding_crops.where(bedding_crops > 0)
     bedding_crops.dropna(inplace= True)
@@ -30,6 +48,20 @@ def assign_bedding(harvest_stores, animals_on_farm):
 
 
 def apply_stocking_limits(animals_on_farm):
+    """
+    Reduce the amount of animals on farm based on pasture space available.
+
+    Parameters
+    ----------
+    animals_on_farm : pd.Series
+        Keeps track of which animals are on the farm and in what amount they
+        are present.
+
+    Returns
+    -------
+    None.
+
+    """
     herd_size = sum(animals_on_farm * gd.animal_data['livestock_units'])
     can_support = gd.livestock_units_max
     while herd_size > can_support:
